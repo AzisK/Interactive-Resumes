eb4771cad6707f3b585c7eaa08e5dad4feb12994
AzisK <azuolas.krusna@yahoo.com>
2017-10-15 11:51:35 +0300

    Add plus sign next to resume points and change the color of hypertext

--- js/components/list.js
+++ js/components/list.js
@@ -1,10 +1,12 @@
+var plusSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="feather"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>';
+
 var list = function (list) {
   var e = list;
   var template = '';
   for (var i = 0; i < e.length; i++) {
     template +=
       '<div class="active-mobile">'
-        + '<h3>' + ': ' + e[i].name + ' @ ' + e[i].placeLink
+        + plusSvg + '<h3>' + e[i].name + ' @ ' + e[i].placeLink
         + e[i].place + '</a>' + e[i].time + '</h3>'
       + '</div>';
   }
